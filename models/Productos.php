6a98242857e733becd92da7d834f362cb208b7ae
joseph501 <abelardo_d_501@hotmail.com>
2021-11-04 12:31:33 -0500

    productossss

--- models/Productos.php
+++ models/Productos.php
@@ -1,16 +1,97 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of Productos
- *
- * @author User
- */
 class Productos {
-    //put your code here
+          
+    private $IdProducto;
+    private $IdProveedor;
+    private $IdCategoria;
+    private $nombre;
+    private $cantidad;
+    private $unidadmedida;
+    private $preciocompra;
+    private $precioventa;
+    private $descripcion;
+    private $estado;
+    
+    function getIdProducto() {
+        return $this->IdProducto;
+    }
+
+    function getIdProveedor() {
+        return $this->IdProveedor;
+    }
+
+    function getIdCategoria() {
+        return $this->IdCategoria;
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getCantidad() {
+        return $this->cantidad;
+    }
+
+    function getUnidadmedida() {
+        return $this->unidadmedida;
+    }
+
+    function getPreciocompra() {
+        return $this->preciocompra;
+    }
+
+    function getPrecioventa() {
+        return $this->precioventa;
+    }
+
+    function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    function getEstado() {
+        return $this->estado;
+    }
+
+    function setIdProducto($IdProducto) {
+        $this->IdProducto = $IdProducto;
+    }
+
+    function setIdProveedor($IdProveedor) {
+        $this->IdProveedor = $IdProveedor;
+    }
+
+    function setIdCategoria($IdCategoria) {
+        $this->IdCategoria = $IdCategoria;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setCantidad($cantidad) {
+        $this->cantidad = $cantidad;
+    }
+
+    function setUnidadmedida($unidadmedida) {
+        $this->unidadmedida = $unidadmedida;
+    }
+
+    function setPreciocompra($preciocompra) {
+        $this->preciocompra = $preciocompra;
+    }
+
+    function setPrecioventa($precioventa) {
+        $this->precioventa = $precioventa;
+    }
+
+    function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+    
 }
